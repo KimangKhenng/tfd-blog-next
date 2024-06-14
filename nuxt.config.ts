610a70ebcca6fd28650cc299a00327c51c662f2d
Ubuntu@3060 <kimangkheang@gmail.com>
2024-06-15 00:29:01 +0700

    Config tailwind, i18n, content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/i18n",
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxtjs/google-fonts"
+  ],
+  googleFonts: {
+    families: {
+      'Source Sans Pro': true,
+      Kantumruy: true
+
+    }
+  }
 })
